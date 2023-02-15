087d72925b4241eaf52a1bbf96985eceb5450192
Yashodhan <yashodhanvashisht@gmail.com>
2023-02-15 20:05:13 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# InstantUpload
+A Instagram Bot that uploads pictures, writes caption including hashtags from a local directory in your machine. Here: https://www.instagram.com/livelikezen/ 
+
+Description:
+A simple yet elegant Instagram Bot that creates posts and upload it to your account from a local folder in your machine. 
+You have to specify a folder from where it will upload images (you can keep adding pictures later as well), then, it writes caption, hashtags, current day etc for your post and 
+upload the contents of the selected folder every time you run the script.
+
+Technologies used:
+Python -->
+	Instagram Bot, Pillow, cv2, other basic libraries
+
+Folders and why's:
+Script >> This folder contains the main.py, backend.py (which contains account info i.e username, password, hastags)
+Photos >> This folder contains: "Original post" (which contains all the images that will be posted), "Already Posted" (To keep a track of uploaded pictures)
+instaupload.bat >> You can run the script directly from the "Windows Run"
+
+Problems and wants:
+1. The script can run in the background 24 and pictures can be uploaded on a once or several times according to the needs of the user.
+2. Config folder gets created in Scripts and causes problem.
+
+This was my first project with Python and I learned a lot of stuff. You can check out the instagram account that I am maintaining with this script, here: https://www.instagram.com/livelikezen/ 
+Looking forward to any pull requests and requests
+
+
---
+++ backend.py
@@ -0,0 +1,75 @@
+# Account Info
+info = {
+
+    'username': 'livelikezen',
+    'password': 'password'
+
+}
+
+# Popular hashtags
+tags = (
+
+    '''
+#instagram #instagood #love #like #follow 
+#photography #photooftheday #instadaily #likeforlikes 
+#picoftheday #fashion #bhfyp #beautiful #instalike #me 
+#likes #followforfollowback #smile #followme #myself #art 
+#photo #happy #style #likeforfollow #life #l #nature #followers #bhfyp
+    ''',
+    '''
+#insta #model #likeforlike #followback #cute #beauty 
+#comment #india #followforfollow #travel #photographer 
+#girl #lifestyle #motivation #tiktok #selfie #quotes #likesforlikes 
+#photoshoot #music #inspiration #instamood #instapic #portrait #makeup
+#naturephotography #loveyourself #fun #memes #ig
+    ''',
+    '''
+#follow me #follow #followforfollow
+#followback #followers #follow4follow #followher 
+#follower#followhim#followall#followbackteam#followbackalways
+#follows#followgram#followalways#tagblender#followmefollowyou
+#following#followstagram#follownow#ifollowback#followus
+#followmeback#followforlike#followmeplease
+#followshoutoutlikecomment#followbackinstantly#f4f#ifollo#followyou
+    ''',
+    '''
+#art#tagblender#artist#artistic#artists#arte#dibujo
+#myart#artwork#illustration#graphicdesign#graphic#color
+#colour#colorful#painting#drawing#drawings#markers#paintings#watercolor
+#watercolour#ink#creative#sketch#sketchaday#pencil#cs6#photoshop#beautiful
+    ''',
+    '''
+#love#instagood#me#cute#tbt#photooftheday
+#instamood#iphonesia#tweegram#picoftheday#igers#girl
+#beautiful#instadaily#summer#instagramhub#iphoneonly
+#follow#igdaily#bestoftheday
+#happy#picstitch#tagblender#jj#sky#nofilter#fashion#followme#fun#su
+    ''',
+    '''
+#love #art #follow #instadaily #instalike #life #likeforlike 
+#igers #bestoftheday #sun #sunset #photooftheday #photography 
+#repost #photo #follow4follow #lifestyle #followforfollow #bestoftheday#workout
+#f4f#nofilter#london#goals#blackandwhite#blue#swag#health#party#night#landscape#nyc
+#happiness#pink#lol#foodporn#newyork#fitfam#awesome#fashionblogger#Halloween#Home#fall#paris
+    ''',
+    '''
+#insta #model #likeforlike #followback #cute #beauty #comment #india 
+#followforfollow #travel #photographer #girl #lifestyle #motivation 
+#tiktok #selfie #quotes #likesforlikes #photoshoot #music #inspiration 
+#instamood #instapic #portrait #makeup #naturephotography #loveyourself #fun #memes #ig
+    ''',
+    '''
+#art#tagblender#artist#artistic#artists#arte#dibujo#myart#artwork#illustration#graphicdesign
+#graphic#color#colour#colorful#painting#drawing#drawings#markers#paintings#watercolor#watercolour
+#ink#creative#sketch#sketchaday#pencil#cs6#photoshop#beautiful#photographer#gym#artist#girls#vscocam#autumn
+#pretty#luxury#instapic#black#sunset#funny#sky#blogger#hot#healthy#work
+
+    ''',
+    '''
+#love#instagood#photooftheday#fashion#Beautiful#like4like#picoftheday#art#happy#photography#instagram
+#followme#style#follow#instadaily#travel#life#cute#fitness#nature#beauty#girl#fun#photo#amazing#likeforlike
+#instalike#Selfie#smile#me#lifestyle#model#follow4follow#music#friends#motivation#like#food#inspiration#Repost
+#summer#design#makeup#TBT#followforfollow#ootd#Family#l4l#cool#igers#TagsForLikes#hair#instamood#sun#vsco#fit#beach
+
+    '''
+)
